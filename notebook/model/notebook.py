dc1c1954798f1fee35fcf044754a7bb90fdb7ce5
USUARIO <jberrio884@soyudemedellin.edu.co>
2024-04-03 20:31:13 -0500

    Work done

--- notebook/model/notebook.py
+++ notebook/model/notebook.py
@@ -1,1 +1,51 @@
+import dataclasses
 from datetime import datetime
+
+
+@dataclasses.dataclass
+class Note:
+    def __init__(self, code: int, title: str, text: str, importance: str):
+        self.HIGH: str = "HIGH"
+        self.MEDIUM: str = "MEDIUM"
+        self.LOW: str = "LOW"
+        self.code: int = code
+        self.title: str = title
+        self.text: str = text
+        self.importance: str = importance
+        self.creation_date: datetime = dataclasses.field(default_factory=datetime.now)
+        self.tags: list[str] = dataclasses.field(default_factory=list)
+
+    def __str__(self) -> str:
+        return f"Code: {self.code} \nCreation date: {self.creation_date} \n{self.title}: {self.text}"
+
+    def add_tag(self, tag: str):
+        if tag not in self.tags:
+            self.tags.append(tag)
+
+
+@dataclasses.dataclass
+class Notebook:
+    def __init__(self):
+        self.note_counter = 0
+        self.tags = None
+        self.notes = None
+
+    def add_note(self, title: str, text: str, importance: str):
+        self.note_counter += 1
+        note = Note(
+            code=self.note_counter,
+            title=title,
+            text=text,
+            importance=importance
+        )
+        self.notes.append(note)
+        return self.note_counter
+
+    def important_notes(self) -> list[Note]:
+        return [note for note in self.notes if note.importance == "HIGH" or note.importance == "MEDIUM"]
+
+    def tags_note_count(self) -> dict[str, int]:
+        tag_count = {}
+        for tag in self.tags:
+            tag_count[tag] = tag_count.get(tag, 0) + 1
+        return tag_count
